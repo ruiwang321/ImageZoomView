27c62b6636af7e54ecbe19be5a4ab850749c6d79
ruiwang <ruiwang@ruiwangdeimac.local>
2016-08-24 08:48:21 +0800

    Initial Commit

---
+++ ImageZoomViewDemoTests/ImageZoomViewDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  ImageZoomViewDemoTests.m
+//  ImageZoomViewDemoTests
+//
+//  Created by ruiwang on 16/8/24.
+//  Copyright © 2016年 ruiwang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ImageZoomViewDemoTests : XCTestCase
+
+@end
+
+@implementation ImageZoomViewDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
